1763b2576252400cbdc5dbd96468ccd02bcf0fc9
Linz Pierre <pierre.linz@orange.fr>
2024-02-08 12:50:20 +0100

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAH

--- image_locker_react_app/src/App.tsx
+++ image_locker_react_app/src/App.tsx
@@ -1,21 +1,8 @@
-import { Router, Route, Switch } from "wouter";
 import React from "react";
 import ReactDOM from "react-dom";
 
 import Home from "./Home";
-import Lock from "./Lock";
-import Login from "./Login";
-import Register from "./Register";
 
-const App = () => (
-  <Router>
-    <Switch>
-      <Route path="/" component={Home} />
-      <Route path="/login" component={Login} />
-      <Route path="/register" component={Register} />
-      <Route path="/lock" component={Lock} />
-    </Switch>
-  </Router>
-);
+const App = () => <Home />;
 
 ReactDOM.render(<App />, document.getElementById("root"));
